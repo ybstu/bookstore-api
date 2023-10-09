93e4c01534d8ee710a7ee6f0846012c93c49fe5e
potatossium <zhangyingzhuchn@gmail.com>
2023-10-09 18:42:50 +1300

    Add book management api

--- index.js
+++ index.js
@@ -6,37 +6,45 @@ let logger = require("morgan");
 let cookieParser = require("cookie-parser");
 const port = 3001;
 
-let index = express();
+let usersRouter = require("./routes/users");
+let booksRouter = require("./routes/books");
+let authRouter = require("./routes/auth");
+
+let app = express();
 
 // view engine setup
-index.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
-index.use(cors());
-index.use(logger("dev"));
-index.use(express.json());
-index.use(express.urlencoded({ extended: false }));
-index.use(cookieParser());
-index.use(express.static(path.join(__dirname, "/public")));
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter.router);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-index.use(function (req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-index.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  console.log(err);
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    console.log(err);
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  //res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    //res.render('error');
 });
 
-index.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-module.exports = index;
+module.exports = app;
